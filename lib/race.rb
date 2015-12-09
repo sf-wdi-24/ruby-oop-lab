178c36f2aa88c5af153c1b9ff355b67b251dff9e
stanleyyork <stanleyyork@gmail.com>
2015-12-09 14:21:57 -0800

    initial commit

--- lib/race.rb
+++ lib/race.rb
@@ -2,6 +2,23 @@ require_relative "car"
 
 class Race
 
-  # write Race class code here
+  def initialize
+  	@cars = [Car.new,Car.new]
+  	@cars[0].accelerate(Random.rand(100))
+  	@cars[1].accelerate(Random.rand(100))
+  	@cars.sort_by {|obj| obj.speed}
+  end
+
+  def cars
+  	@cars
+  end
+
+  def winner
+  	@cars[0]
+  end
+
+  def loser
+  	@cars[1]
+  end
 
 end
