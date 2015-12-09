2f5da40f8cfe3406f2c7d260b5632a498fd187a0
stevennoble78 <stevennoble78@hotmail.com>
2015-12-09 15:17:41 -0800

    tests pass

--- lib/race.rb
+++ lib/race.rb
@@ -1,7 +1,28 @@
 require_relative "car"
 
 class Race
+	attr_reader :cars, :winner, :loser
+	def initialize
+		@cars = Array.new()
+		2.times do |i|
+			@cars.push(Car.new)
+			@cars[i].accelerate(1+rand(100))
+		end
+	end
 
-  # write Race class code here
+	def winner
+		if @cars[0].speed > @cars[1].speed
+			@winner = @cars[0]
+		else
+			@winner = @cars[1]
+		end
+	end
 
+	def loser
+		if @cars[0].speed > @cars[1].speed
+			@loser = @cars[1]
+		else
+			@loser = @cars[0]
+		end
+	end
 end
