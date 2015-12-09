520dcdcb110f2a4c70dea9060fa47aeb283a15f4
mrockway <mrockway@gmail.com>
2015-12-09 15:28:27 -0800

    completed race

--- lib/race.rb
+++ lib/race.rb
@@ -1,7 +1,28 @@
 require_relative "car"
 
 class Race
+	attr_reader :cars
+	def initialize
+		car_1 = Car.new
+		car_2 = Car.new
+		@cars = [car_1, car_2]
+		cars[0].accelerate(rand(0..100)+1)
+		cars[1].accelerate(rand(0..100)+1)
+	end
 
-  # write Race class code here
+	def winner
+		if (cars[0].speed > cars[1].speed)
+			cars[0] 
+		else
+			cars[1]
+		end
+	end
 
+	def loser
+		if (cars[0].speed > cars[1].speed)
+			cars[1]
+		else 
+			cars[0]
+		end
+	end
 end
